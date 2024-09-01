38625697118d9fe61c3ccda8bdc47529ee7d9327
tate379 <yayoi_tateda@mail.toyota.co.jp>
2024-09-02 00:54:23 +0900

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 'use strict'
 
+//イベントの追加
 document.getElementById('searchForm').addEventListener('submit', function(event) {
   event.preventDefault();
+  //カテゴリーの取得
   const selectedCategory = document.getElementById('categorySelect').value;
-  const selectedSize = document.getElementById('sizeSelect').value;
+  //種類の取得
+  const selectedSize = document.getElementById('typeSelect').value;
+  //関数の実行
   openReport(selectedCategory, selectedSize);
 });
 
-function openReport(category, size) {
+
+function openReport(category, type) {
   const filePaths = {
     blueprints: {
       carA: './帳票/設計図_車種A.pdf',
@@ -25,12 +30,15 @@ function openReport(category, size) {
       carC: './帳票/部品図_車種C.pdf'
     }
   };
-
-  const filePath = filePaths[category] ? filePaths[category][size] : null;
+  //ファイルがあるか調べる、ない場合はnullにする
+  const filePath = filePaths[category] ? filePaths[category][type] : null;
   if (filePath) {
+    //ファイルを開く
     window.open(filePath, '_blank');
   } else {
+    //htmlのresultsに表示
     const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '<p>帳票が選択されていません。</p>';
+    resultsDiv.innerHTML = '<p>帳票が選択されていないか、存在しません。</p>';
   }
 }
+
